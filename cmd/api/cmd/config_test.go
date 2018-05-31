f77da63db0e6f169d8c4034428a33dec019f296a
odd-man <nb_odd_man@163.com>
2018-05-31 13:47:37 +0800

    fixed unitest

--- cmd/api/cmd/config_test.go
+++ cmd/api/cmd/config_test.go
@@ -10,17 +10,33 @@ import (
 )
 
 func Test_GetConfigFromFile(t *testing.T) {
-	filepath := "./app.json"
+	filepath := "./config/app.json"
 	config, err := GetConfigFromFile(filepath)
 	if err != nil {
-		t.Errorf("app.json %v", config)
+		t.Fatalf("GetConfigFromFile %s %v", filepath, err)
+	}
+}
+
+func Test_GetConfigFromFileBad(t *testing.T) {
+	filepath := "./config/app-bad.json"
+	config, err := GetConfigFromFile(filepath)
+	if err == nil {
+		t.Fatalf("GetConfigFromFile %s %v", filepath, err)
 	}
 }
 
 func Test_LoadConfigFromFile(t *testing.T) {
-	filepath := "./app.json"
+	filepath := "./config/app.json"
 	config, err := LoadConfigFromFile(filepath)
 	if err != nil {
-		t.Errorf("app.json %v", config)
+		t.Fatalf("LoadConfigFromFile %s %v", filepath, err)
+	}
+}
+
+func Test_LoadConfigFromFileBad(t *testing.T) {
+	filepath := "./config/app-bad.json"
+	config, err := LoadConfigFromFile(filepath)
+	if err == nil {
+		t.Fatalf("LoadConfigFromFile %s %v", filepath, err)
 	}
 }
